fbdb977f3f91542302c414050fad0444406b6593
eieilwin <eiei77448@gmail.com>
2023-07-17 17:23:55 +0630

    owner ;

--- resources/views/properties/create.blade.php
+++ resources/views/properties/create.blade.php
@@ -81,13 +81,13 @@
                 <div class="col-md-2">
                     <div class="form-group">
                         <strong>Bank Loan:</strong>
-                        {!! Form::checkbox('bank_loan', '1', false, array('class' => 'form-check-input mt-2')) !!}
+                        {!! Form::checkbox('bank_loan', null, false, array('class' => 'form-check-input mt-2')) !!}
                     </div>
                 </div>
                 <div class="col-md-2">
                     <div class="form-group">
                         <strong>Public Status:</strong>
-                        {!! Form::checkbox('public_status', '1', false, array('class' => 'form-check-input mt-2')) !!}
+                        {!! Form::checkbox('public_status', null, false, array('class' => 'form-check-input mt-2')) !!}
                     </div>
                 </div>
 
